4feab3fcbc0f17a657a05aa47ac9455c0177dd4d
Rejoice <uahomorejoice@gmail.com>
2022-07-10 17:43:39 +0100

    strict mode

--- src/index.js
+++ src/index.js
@@ -11,78 +11,76 @@ const Login = React.lazy(() => import("./Login"));
 const Register = React.lazy(() => import("./Register"));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <React.Suspense
-              fallback={
-                <div
-                  style={{
-                    width: "100%",
-                    height: "100vh",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <Spinner animation="border" style={{ color: "#2E7DD7" }} />
-                </div>
-              }
-            >
-              <App />
-            </React.Suspense>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <React.Suspense
-              fallback={
-                <div
-                  style={{
-                    width: "100%",
-                    height: "100vh",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <Spinner animation="border" style={{ color: "#2E7DD7" }} />
-                </div>
-              }
-            >
-              <Login />
-            </React.Suspense>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <React.Suspense
-              fallback={
-                <div
-                  style={{
-                    width: "100%",
-                    height: "100vh",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <Spinner animation="border" style={{ color: "#2E7DD7" }} />
-                </div>
-              }
-            >
-              <Register />
-            </React.Suspense>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <React.Suspense
+            fallback={
+              <div
+                style={{
+                  width: "100%",
+                  height: "100vh",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Spinner animation="border" style={{ color: "#2E7DD7" }} />
+              </div>
+            }
+          >
+            <App />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <React.Suspense
+            fallback={
+              <div
+                style={{
+                  width: "100%",
+                  height: "100vh",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Spinner animation="border" style={{ color: "#2E7DD7" }} />
+              </div>
+            }
+          >
+            <Login />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <React.Suspense
+            fallback={
+              <div
+                style={{
+                  width: "100%",
+                  height: "100vh",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Spinner animation="border" style={{ color: "#2E7DD7" }} />
+              </div>
+            }
+          >
+            <Register />
+          </React.Suspense>
+        }
+      />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
