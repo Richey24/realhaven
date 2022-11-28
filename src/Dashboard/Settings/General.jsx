3ed100feab7343161c7bed14dfa60ec2c38a18c4
Richey24 <uahomorejoice@gmail.com>
2022-11-28 19:07:58 +0100

    changes

---
+++ src/Dashboard/Settings/General.jsx
@@ -0,0 +1,25 @@
+import "./General.css"
+
+const General = () => {
+    return (
+        <div className="generalDiv">
+            <div className="firstDiv">
+                <div>
+                    <h1>Agency settings</h1>
+                    <h3>Manage your agency settings</h3>
+                </div>
+                <p>Save changes</p>
+            </div>
+            <div className="secondDiv">
+                <div className="firstSec">
+
+                </div>
+                <div className="secondSec">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default General
